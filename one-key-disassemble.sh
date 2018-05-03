deb6dd440a1889e5dc78fb4e56a6c2c3d77637af
Xc2333 <35050861+Xc2333@users.noreply.github.com>
2018-05-03 21:14:21 +0800

    version v2.0

--- one-key-disassemble.sh
+++ one-key-disassemble.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 #one-key-disassemble.sh
@@ -6,16 +7,45 @@
 #Created by Xc233 on 2018/5/3.
 #
 #
+#
+#
 #get the path of Decompile file.
+iaslpath="/usr/bin/iasl"
 read -p "要反编译的文件夹路径是? (请拖动文件夹到这里):  " path
-cd "$path"
+
 x=`ls -l |grep "^-"|wc -l`
 if [[ $x -eq 0 ]];
-        then
-#no .aml to do
-            echo **************这是一个空文件夹,没有aml文件!请重新确认!**************
-            exit 1
-        else
+then
+echo *********************/****************************
+echo *********************/****************************
+echo *这是一个空文件夹,没有aml文件!请确认文件ACPI文件是否放置正确!*
+echo *********************/****************************
+echo *********************/****************************
+exit 1
+else
+echo *********************文件夹正确!****************************
+fi
+
+#remove SSDT-xx.aml
+cd "$path"
+xc="$path/SSDT-x*.aml"
+if [ ! -f "$xc" ];
+then
+rm SSDT-x*.aml
+else
+echo *********************很好,没有动态SSDT文件****************************
+fi
+
+if [ ! -f "$iaslpath" ];
+then
+echo *********************/****************************
+echo *********************/****************************
+echo ********************警告!!!************************
+echo "iasl不存在,请安装,下载地址:https://bitbucket.org/RehabMan/acpica/downloads/"
+echo *********************/****************************
+echo *********************/****************************
+exit 1
+else
 #do work...
 #creat refs.txt
 cd "$path"
@@ -32,9 +62,9 @@ External(_GPE.VHOV,MethodObj,3)
 External(_SB.PCI0.XHC.RHUB.TPLD,MethodObj,2)
 END
 
+
 #use iasl to Compile. Aussme you hava install iasl. If not,install it
 #to /usr/bin
-
 iasl -da -dl -fe refs.txt DSDT.aml SSDT*.aml
 #remove something
 rm refs.txt
