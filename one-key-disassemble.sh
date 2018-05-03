c64a280279d674ad13c999ec2c66bc7f4fd2a844
Xc2333 <35050861+Xc2333@users.noreply.github.com>
2018-05-03 18:04:48 +0800

    add empty file detect

--- one-key-disassemble.sh
+++ one-key-disassemble.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-#ASL.sh
+#one-key-disassemble.sh
 #
 #
 #Created by Xc233 on 2018/5/3.
 #
 #
-#
-#
 #get the path of Decompile file.
-read -p "要反编译的文件夹路径是? (请拖动文件夹到这里,文件夹名称请勿带空格!):  " path
-
+read -p "要反编译的文件夹路径是? (请拖动文件夹到这里):  " path
+cd "$path"
+x=`ls -l |grep "^-"|wc -l`
+if [[ $x -eq 0 ]];
+        then
+#no .aml to do
+            echo **************这是一个空文件夹,没有aml文件!请重新确认!**************
+            exit 1
+        else
+#do work...
 #creat refs.txt
 cd "$path"
 cat <<END >refs.txt
@@ -30,11 +36,13 @@ END
 #to /usr/bin
 
 iasl -da -dl -fe refs.txt DSDT.aml SSDT*.aml
-
+#remove something
 rm refs.txt
 rm *.aml
+rm DumpLog.txt
 
 #done and show information
 clear
 echo ********************************编译完成*************************************
 exit 0
+fi
